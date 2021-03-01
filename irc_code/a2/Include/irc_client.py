fd6a232f8fdbccedc52111e968cc3e5c23d251ef
Pierre-Anthony Moffet <pamoffet@consoltec.ca>
2021-02-28 19:05:24 -0500

    virtual environment, bat file to autorun, client code

---
+++ irc_code/a2/Include/irc_client.py
@@ -0,0 +1,190 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2021
+#
+# Distributed under terms of the MIT license.
+
+"""
+Description:
+
+"""
+import socket
+import select
+
+import asyncio
+import logging
+import sys
+import os
+
+import patterns
+import view
+import argparse
+
+logging.basicConfig(filename='view.log', level=logging.DEBUG)
+logger = logging.getLogger()
+MSGLEN = 1000
+
+
+class IRCClient(patterns.Subscriber):
+
+    def __init__(self):
+        super().__init__()
+        self.username = str()
+        self._run = True
+        self.is_connected = False
+        #create_client_socket(self)
+
+    def __init__(self, nickname, host, port):
+        super().__init__()
+        self.nickname = nickname
+        self.host = host
+        self.port = port
+        self._run = True
+        self.is_connected = False
+        #create_client_socket(self)
+
+    def set_view(self, view):
+        self.view = view
+
+    def update(self, msg):
+        # Will need to modify this
+        if not isinstance(msg, str):
+            raise TypeError(f"Update argument needs to be a string")
+        elif not len(msg):
+            # Empty string
+            return
+        logger.info(f"IRCClient.update -> msg: {msg}")
+        self.process_input(msg)
+
+    def process_input(self, msg):
+        # Will need to modify this
+        
+        if msg.lower().startswith('/connect '):
+            self.add_msg(msg)
+            split_string = msg.split(" ")
+            if len(split_string) > 4:
+                self.username = split_string[1]
+                self.server_host = split_string[2]
+                self.server_port = split_string[3]
+                self.real_name = split_string[4]
+                connect(self)
+
+        if msg.lower().startswith('/msg '):
+            self.add_msg(msg)
+            split_string = msg.split('/msg ', 1)
+            if len(split_string) == 2:
+                send_message(self, msg)
+
+        if msg.lower().startswith('/quit'):
+            # Command that leads to the closure of the process
+            raise KeyboardInterrupt
+
+    def add_msg(self, msg):
+        self.view.add_msg(self.nickname, msg)
+
+    async def run(self):
+        """
+        Driver of your IRC Client
+        """
+        try:
+            while True:
+                self.add_msg("here")
+                if hasattr(self, 'server_socket'):
+                    chunks = []
+                    bytes_recd = 0
+                    while bytes_recd < MSGLEN:
+                        chunk = self.server_socket.recv(min(MSGLEN - bytes_recd, 2048))
+                        if chunk == b'':
+                            raise RuntimeError("socket connection broken")
+                        chunks.append(chunk)
+                        bytes_recd = bytes_recd + len(chunk)
+                    message = b''.join(chunks)
+                    self.add_msg(message)
+                await asyncio.sleep(2)
+
+        except KeyboardInterrupt:
+            print(f"\nServer interrupted, closing socket connections")
+            self.close()
+        except RuntimeError:
+            print(f"\Connection interrupted, closing socket connections")
+            self.close()
+
+    def close(self):
+        # Terminate connection
+        logger.debug(f"Closing IRC Client object")
+        pass
+
+
+    def connect(self):
+        if (hasattr(self, 'nickname')):
+            nick_msg = " ".join(["NICK", self.nickname])
+
+        if hasattr(self, 'username') and hasattr(self, 'server_host') and hasattr(self, 'server_port'):
+            user_msg = " ".join(["USER", self.username, self.server_host, self.server_port])
+
+        if not(hasattr(self, 'server_socket')):
+            create_server_socket(self)
+            
+        msg = ";".join(nick_msg, user_msg)
+        self.server_socket.send(msg)
+        self.is_connected = True
+
+    def send_message(self, msg):
+        if self.is_connected:
+            msg = " ".join(["NOTICE", msg])
+            self.server_socket.send(msg)
+
+    # def create_client_socket(self):
+    #     self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #     self.client_socket.setblocking(False) #server won't block at server_socket.accept()
+    #     self.client_socket.bind((self.host, self.port))
+    #     self.client_socket.listen()
+
+    #     self.potential_reads = list()
+    #     self.potential_reads.append(self.client_socket)
+    #     self.potential_writes = list()
+    #     self.potential_errors = list()
+
+    def create_server_socket(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setblocking(False) #server won't block at server_socket.accept()
+        self.server_socket.connect((self.server_host, self.server_port))
+        self.potential_reads.append(self.server_socket)
+
+
+def set_parser(): 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--nickname', action="store", dest="nickname", default="client_01")
+    parser.add_argument('--host', action="store", dest="host", default="localhost")
+    parser.add_argument('--port', action="store", dest="port", default="1337")
+    return parser  
+
+def main(args):
+    # Pass your arguments where necessary
+    client = IRCClient(args.nickname, args.host, args.port)
+    logger.info(f"Client object created")
+    with view.View() as v:
+        logger.info(f"Entered the context of a View object")
+        client.set_view(v)
+        logger.debug(f"Passed View object to IRC Client")
+        v.add_subscriber(client)
+        logger.debug(f"IRC Client is subscribed to the View (to receive user input)")
+        async def inner_run():
+            await asyncio.gather(
+                v.run(),
+                client.run(),
+                return_exceptions=True,
+            )
+        try:
+            asyncio.run( inner_run() )
+        except KeyboardInterrupt as e:
+            logger.debug(f"Signifies end of process")
+    client.close()
+
+if __name__ == "__main__":
+    parser = set_parser()
+    args = parser.parse_args()
+
+    main(args)
---
+++ irc_code/a2/Include/irc_server.py
@@ -0,0 +1,97 @@
+"""
+SERVER
+"""
+import socket
+import select
+
+class Server:
+
+    def __init__(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setblocking(False) #server won't block at server_socket.accept()
+        host, port = 'localhost', 8081
+        self.server_socket.bind((host, port))
+        self.server_socket.listen()
+
+        #Select vars
+        self.potential_reads = list()
+        self.potential_reads.append(self.server_socket)
+        self.potential_writes = list()
+        self.potential_errors = list()
+        self.outbox = dict()
+        self.messages = dict()
+        self.read_size = 2**10
+
+    def run(self):
+        print("Running server at localhost:8081")
+        try:
+            while True:
+                r_reads, r_writes, r_errors = select.select(self.potential_reads, self.potential_writes, self.potential_errors)
+                for r in r_reads:
+                   
+                    # Server socket accepting clients
+                    if r is self.server_socket:
+                        client_socket, addr = r.accept()
+                        client_socket.setblocking(False)
+                        print("New client @ {}".format(addr))
+                        msg = "Hello client"
+                        self.outbox[client_socket] = "HTTP/1.0 200 OK\r\nContent-Length: {}\r\n\r\n{}".format(len(msg), msg)
+                        self.messages[client_socket] = str()
+                        self.potential_reads.append(client_socket)
+                        self.potential_writes.append(client_socket)
+                    # Reading from clients
+                    else:
+                        data = r.recv(self.read_size).decode('utf-8')
+                        if data:
+                            print(f"Received data: {data}")
+                            self.messages[r] += data
+                            self._parse_message(r)
+                            pass
+                        # Data is empty
+                        else:
+                            self.potential_reads.remove(r)
+                            self.potential_writes.remove(r)
+                            if r is self.outbox:
+                                del self.outbox[r]
+                            if r is self.messages:
+                                del self.messages[r]
+                            print(f"Closed a client socket: {r}")
+                            r.close()
+                for w in r_writes:
+                    msg = self.outbox[w]
+                    if len(msg):
+                        w.send(msg.encode()) # String to bytes
+                        self._clear_outbox(w)
+                for err in r_errors:
+                    self.potential_reads.remove(err)
+                    if err is self.outbox:
+                        del self.outbox[err]
+                    if err is self.messages:
+                        del self.messages[err]
+
+        except KeyboardInterrupt:
+            print(f"\nServer interrupted, closing socket connections")
+            self.close()
+
+    def _clear_outbox(self, client_socket):
+        if client_socket in self.outbox:
+            self.outbox[client_socket] = str()
+
+    def close(self):
+        """
+        Close all readable sockets
+        (including server socket)
+        """
+        # self.server_socket.close() # might not need but faced problems without it
+        for s in self.potential_reads:
+            s.close()
+
+    def _parse_message(self, client):
+        """
+        If message is bigger than the read size then it must be broken up
+        """
+        pass
+
+if __name__ == "__main__":
+    server = Server()
+    server.run()
